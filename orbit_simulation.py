1111d7489b5d7384b0031c401402818a500661c9
NeilNNP5-dev <neilnnp45@gmail.com>
2026-06-24 18:48:59 +0530

    Now every celestial affects gravity (N body gravity). Added planet 3

--- orbit_simulation.py
+++ orbit_simulation.py
@@ -25,28 +25,31 @@ class Body:
         self.radius = radius
         self.mass = mass
         self.trail = []
-planet1 = Body(500,300,6.1,0,(0,0,255),10,1)
+planet1 = Body(500,300,6.1,0,(0,0,255),10,10)
 sun = Body(500,500,0,0,(255,255,0),30,7500)
-planet2 = Body(500,100,4.3,0,(255,0,0),15,5)
-bodies = [sun,planet1,planet2]
+planet2 = Body(500,100,4.3,0,(255,0,0),15,50)
+planet3 = Body(500,200,5,0,(180,0,255),12.5,30)
+bodies = [sun,planet1,planet2,planet3]
 G= 1
-ax = 1
-ay = 1
 def update_physics(bodies):
-    sun = bodies[0]
     for body in bodies:
-        if body is sun:
-            continue
-        dx = sun.x - body.x
-        dy = sun.y - body.y 
-        distance = math.sqrt(dx**2 + dy**2 +100)
-        a = G*sun.mass/distance**2
-        ax = a*dx/distance
-        ay = a*dy/distance
-        body.vx += ax
-        body.x +=  body.vx
-        body.vy += ay  
-        body.y += body.vy
+        total_ax = 0
+        total_ay = 0
+        for other_body in bodies:
+            if body is other_body:
+                continue
+            dx = other_body.x - body.x
+            dy = other_body.y - body.y 
+            distance = math.sqrt(dx**2 + dy**2 +100)
+            a = G*other_body.mass/distance**2
+            ax = a*dx/distance
+            ay = a*dy/distance
+            total_ax += ax
+            total_ay += ay
+        body.vx += total_ax
+        body.vy += total_ay
+        body.x += body.vx
+        body.y += body.vy         
         body.trail.append((body.x, body.y))
         if len(body.trail)>MAX_TRAIL:
             body.trail.pop(0)
@@ -57,15 +60,9 @@ while running:
     screen.fill((0,0,0))
     update_physics(bodies)
     for body in bodies:
-        if body is not sun:
-            for i in range(len(body.trail)-1):
+        for i in range(len(body.trail)-1):
                 pygame.draw.line(screen,body.color,body.trail[i],body.trail[i+1],2)
         pygame.draw.circle(screen,(body.color),(body.x,body.y),body.radius)
-        if body.x+body.radius>=WIDTH or body.x-body.radius<=0:
-           body.vx*= -1
-        if body.y+body.radius>=HEIGHT or body.y-body.radius<=0:
-           body.y = HEIGHT-body.radius
-           body.vy*= -1
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
